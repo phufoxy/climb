6e479f9860cceb0f5562d3bfd46293f7df6f6218
vanphudhsp2015 <vanphudhsp2015@gmail.com>
2018-07-31 23:22:35 +0700

    abc

--- src/app/components/about/about.component.ts
+++ src/app/components/about/about.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+declare var $: any;
 
 @Component({
   selector: 'app-about',
@@ -8,8 +9,47 @@ import { Component, OnInit } from '@angular/core';
 export class AboutComponent implements OnInit {
 
   constructor() { }
-
   ngOnInit() {
+    this.Header();
+  }
+  Header() {
+    window.onscroll = function () {
+      myScrolltop();
+    }
+    $(window).on('mousewheel', function () {
+      $('html,body').stop();
+    });
+
+    $(window).scroll(function (e) {
+      if ($(this).scrollTop() > 100) {
+        $('.upscroll').fadeIn();
+      } else {
+        $('.upscroll').fadeOut();
+      }
+    });
+
+    $('#upscroll').click(function () {
+      $('html, body').animate({ scrollTop: 0 }, 2000);
+      return false;
+    });
+
+    //
+    var header = document.getElementById('header');
+    // console.log(header.nodeType);
+    var sticky = header.offsetTop;
+
+    function myScrolltop() {
+      if (window.pageYOffset > sticky) {
+        header.classList.add('sticky');
+      } else {
+        header.classList.remove('sticky');
+      }
+      if (document.body.scrollTop > 500 || document.documentElement.scrollTop > 500) {
+        document.getElementById("upscroll").style.display = "block";
+      } else {
+        document.getElementById("upscroll").style.display = "none";
+      }
+    }
   }
 
 }
